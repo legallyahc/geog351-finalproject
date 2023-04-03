79b8902cec25c1117fcf24668d1db93d62eab26f
AHC <legallyahc@users.noreply.github.com>
2023-04-03 10:22:51 -0400

    added spatial data, more presentation jazz

--- Data import.R
+++ Data import.R
@@ -59,6 +59,11 @@ vars <- vars %>%
             `Bachelor's degree and level 7 qualification`, `Post-graduate and honours degrees`,
             `Master's degree`, `Doctorate degree`))
 
+vars <- vars %>%
+  mutate(
+    Area_code = as.character(Area_code)
+  )
+
 write_csv(vars, "census.csv")
 
 
